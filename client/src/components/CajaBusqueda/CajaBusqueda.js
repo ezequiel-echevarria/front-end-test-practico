974810915d7ea2a21c855e5e9b8838385591a921
Ezequiel Echevarria <eechevarria@hexacta.com>
2020-10-28 10:12:31 -0300

    Se crearon pages y components bases

---
+++ client/src/components/CajaBusqueda/CajaBusqueda.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CajaBusqueda.module.sass';
+
+const CajaBusqueda = () => (
+  <div className={styles.CajaBusqueda}>
+    CajaBusqueda Component
+  </div>
+);
+
+CajaBusqueda.propTypes = {};
+
+CajaBusqueda.defaultProps = {};
+
+export default CajaBusqueda;
