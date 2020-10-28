357c0aee83855fe697cc0fd4779e40e195a005b5
Ezequiel Echevarria <eechevarria@hexacta.com>
2020-10-28 12:06:43 -0300

    Se comenzo con la implementacion del breadcrumb y el items

--- client/src/components/Breadcrumb/Breadcrumb.js
+++ client/src/components/Breadcrumb/Breadcrumb.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Breadcrumb.module.sass';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Breadcrumb.module.sass";
 
-const Breadcrumb = () => (
-  <div className={styles.Breadcrumb}>
-    Breadcrumb Component
-  </div>
-);
-
+const Breadcrumb = ({ categories }) => {
+  return (
+    <div className={styles.Breadcrumb}>
+      <nav class="breadcrumb has-succeeds-separator" aria-label="breadcrumbs">
+        <ul>
+          {categories.map((item, index) => {
+            return (
+              <li
+                key={item.toString()}
+                className={
+                  categories.length - 1 === index
+                    ? "is-active has-text-weight-bold"
+                    : ""
+                }
+              >
+                <p className={styles.BreadcrumbItem}>{item}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 Breadcrumb.propTypes = {};
 
-Breadcrumb.defaultProps = {};
+Breadcrumb.defaultProps = {
+  category: [],
+};
 
 export default Breadcrumb;
--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Items.module.sass';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Items.module.sass";
+import { useLocation } from "react-router-dom";
+import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
+import CajaBusqueda from "../../components/CajaBusqueda/CajaBusqueda";
+import { useHistory } from "react-router-dom";
 
-const Items = () => (
-  <div className={styles.Items}>
-    Items Component
-  </div>
-);
+const Items = () => {
+  const history = useHistory();
+  const onSubmitHandler = (value) => history.push(`items?query=${value}`);
+
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  const queryValue = query.get("query");
+
+  const categories = ["uno", "dos", "tres"];
+
+  return (
+    <>
+      <CajaBusqueda
+        query={queryValue}
+        onSubmit={onSubmitHandler}
+      ></CajaBusqueda>
+      <div className={styles.Items}>
+        <div className="container">
+          <Breadcrumb categories={categories}></Breadcrumb>
+          <section className="section has-background-white">
+            <div className="container">
+              <h1 className="title">Section</h1>
+              <h2 className="subtitle">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Debitis quos nisi, ullam, numquam soluta natus odit esse alias
+                ea expedita sapiente repellendus veritatis, reiciendis quibusdam
+                rem blanditiis? Earum, tempora commodi!
+              </h2>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
 
 Items.propTypes = {};
 
